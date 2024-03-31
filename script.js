136cbb27b2cba21eebc3453ef44d4966b14aca57
Amit Tambulkar <amittambulkar104@gmail.com>
2024-03-31 15:41:33 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,79 @@
+'use strict';
+
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+
+///////////////////////////////////////
+const loadCountry = function (data, className = '') {
+  let html = `
+  <article class="country ${className}">
+    <img class="country__img" src="${data.flags.png}" />
+    <div class="country__data">
+       <h3 class="country__name">${data.name.common}</h3>
+       <h4 class="country__region">${data.region}</h4>
+       <p class="country__row"><span>👫</span>${(
+         data.population / 10000000
+       ).toFixed(1)}M people</p>
+       <p class="country__row"><span>🗣️</span>${
+         Object.values(data.languages)[0]
+       }</p>
+       <p class="country__row"><span>💰</span>${
+         Object.keys(data.currencies)[0]
+       } </p>
+     </div>
+  </article>
+  `;
+
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+// const getCountry = function (country) {
+//   const request = new XMLHttpRequest();
+//   request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+//   request.send();
+
+//   request.addEventListener('load', function () {
+//     const [data] = JSON.parse(this.responseText);
+//     console.log(data);
+
+//     loadCountry(data);
+
+//     const neibhour = data.borders?.[0];
+
+//     if (!neibhour) return;
+
+//     const request2 = new XMLHttpRequest();
+//     request2.open('GET', `https://restcountries.com/v3.1/alpha/${neibhour}`);
+//     request2.send();
+//     request2.addEventListener('load', function () {
+//       const [data2] = JSON.parse(this.responseText);
+//       console.log(data2);
+
+//       loadCountry(data2, 'neighbour');
+//     });
+//   });
+// };
+
+// getCountry('bharat');
+// // getCountry('usa');
+
+// ------------ Fetch() and Promise -------------
+
+const getCountryData = function (country) {
+  fetch(`https://restcountries.com/v3.1/name/${country}`)
+    .then(response => response.json())
+    .then(data => {
+      loadCountry(data[0]);
+      console.log(data);
+
+      const neighbour = data[0].borders?.[0];
+      console.log(neighbour);
+      if (!neighbour) return;
+
+      return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`);
+    })
+    .then(response => response.json())
+    .then(data => loadCountry(data[0], 'neighbour'));
+};
+
+getCountryData('bharat');
